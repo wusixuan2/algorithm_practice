16e3dd9bbf90a39a252eda7300bdd8c4bf5d4438
Sixuan Wu <wusixuan2@gmail.com>
2019-11-26 21:30:00 -0500

    add sort color

--- twopointers.py
+++ twopointers.py
@@ -126,3 +126,35 @@ class Solution:
 
         self.quickSort(A, start, right)
         self.quickSort(A, left, end)
+################################################################
+# Given an array of n objects with k different colors (numbered from 1 to k), sort them so that objects of the same color are adjacent, with the colors in the order 1, 2, ... k.
+
+class Solution:
+    """
+    @param colors: A list of integer
+    @param k: An integer
+    @return: nothing
+    """
+    def sortColors2(self, colors, k):
+        self.sort(colors, 1, k, 0, len(colors) - 1)
+
+    def sort(self, colors, color_from, color_to, index_from, index_to):
+        if color_from == color_to or index_from == index_to:
+            return
+
+        color = (color_from + color_to) // 2
+
+        left, right = index_from, index_to
+        while left <= right:
+            while left <= right and colors[left] <= color:
+                left += 1
+            while left <= right and colors[right] > color:
+                right -= 1
+            if left <= right:
+                colors[left], colors[right] = colors[right], colors[left]
+                left += 1
+                right -= 1
+
+        self.sort(colors, color_from, color, index_from, right)
+        self.sort(colors, color + 1, color_to, left, index_to)
+
