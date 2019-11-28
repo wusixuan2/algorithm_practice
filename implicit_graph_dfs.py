bb98c3f6d30a025f051e8a1f94d0bfa0f7301312
Sixuan Wu <wusixuan2@gmail.com>
2019-11-28 09:32:24 -0500

    add word pattern I II

--- implicit_graph_dfs.py
+++ implicit_graph_dfs.py
@@ -30,3 +30,67 @@ class Solution:
 
         for letter in KEYBOARD[digits[index]]:
             self.dfs(digits, index + 1, string + letter, results)
+
+    # Word Pattern
+    """
+    @param pattern: a string,denote pattern string
+    @param str: a string,denote matching string
+    @return: return an boolean,denote whether the pattern string and the matching string match or not
+    """
+    def wordPattern(self, pattern, teststr):
+        words = teststr.split()
+
+        if len(words) != len(pattern):
+            return False
+
+        c_2_w, w_2_c = {}, {}
+        for c, w in zip(pattern, words):
+            if c in c_2_w and c_2_w[c] != w:
+                return False
+            c_2_w[c] = w
+
+            if w in w_2_c and w_2_c[w] != c:
+                return False
+            w_2_c[w] = c
+
+        return True
+
+    # Word Pattern II - dfs
+    """
+    @param pattern: a string,denote pattern string
+    @param str: a string, denote matching string
+    @return: a boolean
+    """
+    def wordPatternMatch(self, pattern, string):
+        return self.is_match(pattern, string, {}, set())
+
+    def is_match(self, pattern, string, mapping, used):
+        if not pattern:
+            return not string
+
+        char = pattern[0]
+        if char in mapping:
+            word = mapping[char]
+            if not string.startswith(word):
+                return False
+            return self.is_match(pattern[1:], string[len(word):], mapping, used)
+
+        for i in range(len(string)):
+            word = string[:i + 1]
+            if word in used:
+                continue
+
+            used.add(word)
+            mapping[char] = word
+
+            if self.is_match(pattern[1:], string[i + 1:], mapping, used):
+                return True
+
+            del mapping[char]
+            used.remove(word)
+
+        return False
+
+sol = Solution()
+sol.wordPatternMatch("bab","blueredblue")
+
