db6c7f147d23cac87a9b1a049d313f66457b36fe
Sixuan Wu <wusixuan2@gmail.com>
2019-12-01 12:03:44 -0500

    Implement Queue by Two Stacks

--- HashHeap.py
+++ HashHeap.py
@@ -89,3 +89,35 @@ class Solution:
             h.next = ListNode(heappop(heap))
             h = h.next
         return dummy.next
+
+########################## Implement Queue by Two Stacks ##########################
+class MyQueue:
+    def __init__(self, ):
+        # do intialization if necessary
+        self.stack1 = []
+        self.stack2 = []
+
+    """
+    @param: element: An integer
+    @return: nothing
+    """
+    def push(self, element):
+        self.stack1.append(element)
+
+    """
+    @return: An integer
+    """
+    def pop(self, ):
+        self.top()
+        return self.stack2.pop()
+
+
+    """
+    @return: An integer
+    """
+    def top(self, ):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+
+        return self.stack2[-1]
