810d2c841e5b4dd741dff5aa2b173f7c3198b2d3
Sixuan Wu <wusixuan2@gmail.com>
2019-12-07 18:18:15 +0100

    generatePalindromes

--- palindrome.py
+++ palindrome.py
@@ -17,4 +17,42 @@ def longestPalindromeSubseq(s):
       dp[j] = current
   return dp[len(s)-1]
 
-longestPalindromeSubseq('bbbab')
+# longestPalindromeSubseq('bbbab')
+
+################ 回文排列II  · Palindrome Permutation II ################
+class Solution:
+    """
+    @param s: the given string
+    @return: all the palindromic permutations (without duplicates) of it
+    """
+    def generatePalindromes(self, s):
+        import collections
+        counter = collections.Counter(s)
+        odds = list(filter(lambda x: x % 2, counter.values()))
+        if len(odds) > 1:
+            return []
+        baseStr, mid = self.preProcess(counter)
+        return self.backTracking(baseStr, 0, mid, [baseStr + mid + baseStr[::-1]])
+
+    def preProcess(self, counter):
+        baseStr = mid = ""
+        for char in counter:
+            if counter[char] % 2:
+                mid = char
+            baseStr += char*(counter[char]//2)
+        return baseStr, mid
+
+    def backTracking(self, s, idx, mid, ans):
+        if idx == len(s) - 1:
+            return ans
+        for i in range(idx, len(s)):
+            if i >= 1 and s[i] == s[i-1] == s[idx]:
+                continue #no need to go deeper if swap would be the same
+            #Swap s[idx] with s[i]
+            if i != idx:
+                permu = s[:idx] + s[i] + s[idx+1:i] + s[idx] + s[i+1:]
+                ans.append(permu + mid + permu[::-1])
+            else:
+                permu = s
+            self.backTracking(permu, idx+1, mid, ans)
+        return ans
