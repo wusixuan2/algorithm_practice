fffd47d49c94ad0cf89c3e736ac34df21b2c3dcd
Sixuan Wu <wusixuan2@gmail.com>
2019-12-11 23:19:09 +0100

    Valid Palindrome II

--- palindrome.py
+++ palindrome.py
@@ -96,4 +96,29 @@ class Solution:
         for half_palindrome in set(itertools.permutations(chars))] if len(mid) < 2 else []
 
 sol = Solution()
-print(sol.generatePalindromes('aabbcc'))
+# print(sol.generatePalindromes('aabbcc'))
+
+################ Valid Palindrome II ################
+class Solution:
+    """
+    @param s: a string
+    @return: nothing
+    """
+    def validPalindrome(self, s):
+        left, right = self.twoPointer(s, 0, len(s) - 1)
+        if left >= right:
+            return True
+
+        return self.isPalindrome(s, left + 1, right) or self.isPalindrome(s, left, right - 1)
+
+    def isPalindrome(self, s, left, right):
+        left, right = self.twoPointer(s, left, right)
+        return left >= right
+
+    def twoPointer(self, s, left, right):
+        while left < right:
+            if s[left] != s[right]:
+                return left, right
+            left += 1
+            right -= 1
+        return left, right
