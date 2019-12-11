b50ee188676983163dd2cffbe73884d2f55f445d
Sixuan Wu <wusixuan2@gmail.com>
2019-12-12 06:42:50 +0800

    Palindrome Partitioning using memoization search for function get_is_palindrome

--- palindrome.py
+++ palindrome.py
@@ -193,3 +193,41 @@ class Solution:
 
         memo[s] = partitions
         return partitions
+
+    # 3. 记忆化搜索来实现 get_is_palindrome
+    def partition(self, s):
+        results = []
+        self.dfs(s, 0, [], {}, results)
+        return results
+
+    def generate_solution(self, s, partition):
+        strings = []
+        last_index = -1
+        for i in partition:
+            strings.append(s[last_index + 1: i + 1])
+            last_index = i
+        return strings
+
+    def get_is_palindrome(self, memo, s, i, j):
+        if (i, j) in memo:
+            return memo[(i, j)]
+
+        if i == j:
+            return True
+        if i + 1 == j:
+            return s[i] == s[j]
+
+        memo[(i, j)] = s[i] == s[j] and self.get_is_palindrome(memo, s, i + 1, j - 1)
+        return memo[(i, j)]
+
+    def dfs(self, s, index, partition, memo, results):
+        if index == len(s):
+            results.append(self.generate_solution(s, partition))
+            return
+
+        for i in range(index, len(s)):
+            if not self.get_is_palindrome(memo, s, index, i):
+                continue
+            partition.append(i)
+            self.dfs(s, i + 1, partition, memo, results)
+            partition.pop()
