5e8875a406739bd9775f030e86008991e0350420
Sixuan Wu <wusixuan2@gmail.com>
2020-01-01 14:55:58 -0500

    Coins in a Line II

--- dp3.py
+++ dp3.py
@@ -38,3 +38,32 @@ class Solution:
         while x * (x + 1) / 2 < n:
             x += 1
         return x
+
+class Solution:
+    # 使用滚动数组，加上侯卫东老师在算法强化班中讲解的转移方程
+    # dp[i] = max{values[i] - dp[i+1], values[i] + values[i + 1] - dp[i + 2]}
+
+    # 从一亩三分地copy下面这段解释：
+    # "令dp[i]为"到游戏结束为止, 在i处先拿的人比后拿的人能够多拿的面值".
+    # 如果i处先拿的人只拿1枚, 那么在i处他比对方多拿values[i], 但是在i+1~N处,对手可以比他多拿dp[i+1], 也就是他比对手多拿-dp[i + 1].
+    # 因此从i到N, 他比对方多拿values[i] - dp[i+1];同理, 如果先拿的人拿2枚, 那么能比对方多拿values[i] + values[i + 1] - dp[i + 2].
+    # 取这二者最大值即可得dp[i], 最后看dp[0] 是大于还是小于0即可."
+    """
+    @param values: a vector of integers
+    @return: a boolean which equals to true if the first player will win
+    """
+    def firstWillWin(self, values):
+        # write your code here
+        if not values:
+            return False
+
+        n = len(values)
+        dp = [0] * 3
+        #dp[2] = 0
+
+        for i in range(n-1, -1, -1):
+            dp[i%3] = values[i] - dp[(i+1) % 3]
+            if i + 2 <= n:
+                dp[i%3] = max(dp[i%3], values[i] + values[i+1] - dp[(i+2) % 3])
+
+        return dp[0] >= 0
